2566ff60bf5c7b58fc1f2e6f028c0d6cd3038372
[ykkupakkin] <[csoda.bence@gmail.com]>
2020-03-11 18:22:19 +0100

    add more tape test, snazzy is a beauty

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const test = require('tape');
-//import addNum from '../public/index';
+// import addNum from '../public/index';
 
 const addNum = (a, b) => {
   return a + b;
@@ -13,4 +13,40 @@ test('add 2 numbers', t => {
 
   t.equal(actual, expected);
   t.end();
+});
+
+test('deep equality', t => {
+  t.plan(2);
+
+  t.deepEqual([3, 4, 5], [3, 4, 2 + 3]);
+  t.deepEqual({ a: 7, b: [8, 9] }, { a: 3 + 4, b: [4 * 2].concat(3 * 3) });
+});
+
+test('comparing booleans', t => {
+  t.plan(1);
+
+  t.ok(3 > 4 || 5 > 2);
+});
+
+test('negatives', function (t) {
+  t.plan(3);
+  t.notEqual(1 + 2, 5);
+  t.notDeepEqual([1, 2], [12]);
+  t.notOk(false);
+});
+
+test('empty map', function (t) {
+  [].map(function (x) {
+    t.fail('this callback should never fire');
+  });
+
+  t.end();
+});
+
+test('map with elements', function (t) {
+  t.plan(2);
+
+  [2, 3].map(function (x) {
+    t.pass();
+  });
 });
